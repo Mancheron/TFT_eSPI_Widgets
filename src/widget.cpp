cf915acd3e2268111a4dee8e6fa934a09e2797be
DoccY <alban.mancheron@lirmm.fr>
2022-11-30 16:46:18 +0100

    Add missing copyright notices. Fix some bugs in widget viewports. Add some new methods to widgets.

--- src/widget.cpp
+++ src/widget.cpp
@@ -1,3 +1,86 @@
+/******************************************************************************
+ *                                                                             *
+ *  Copyright © 2022      -- LIRMM/CNRS/UM                                     *
+ *                           (Laboratoire d'Informatique, de Robotique et de   *
+ *                           Microélectronique de Montpellier /                *
+ *                           Centre National de la Recherche Scientifique /    *
+ *                           Université de Montpellier)                        *
+ *                                                                             *
+ *  Auteurs/Authors: Alban MANCHERON  <alban.mancheron@lirmm.fr>               *
+ *                                                                             *
+ *  Programmeurs/Programmers:                                                  *
+ *                   Alban MANCHERON  <alban.mancheron@lirmm.fr>               *
+ *                                                                             *
+ *  -------------------------------------------------------------------------  *
+ *                                                                             *
+ *  Ce fichier fait partie de la librairie TFT_eSPI_Widgets.                   *
+ *                                                                             *
+ *  La librairie TFT_eSPI_Widgets a pour objet de simplifier le développement  *
+ *  d'interfaces graphiques  pour l'uitlisateur  (GUI)  en se basant  sur une  *
+ *  approche par *widgets* (composants graphiques).                            *
+ *                                                                             *
+ *  Ce logiciel est régi par la licence CeCILL-C soumise au droit français et  *
+ *  respectant les principes  de diffusion des logiciels libres.  Vous pouvez  *
+ *  utiliser, modifier et/ou redistribuer ce programme sous les conditions de  *
+ *  la licence CeCILL-C telle que diffusée par le CEA, le CNRS et l'INRIA sur  *
+ *  le site "http://www.cecill.info".                                          *
+ *                                                                             *
+ *  En contrepartie de l'accessibilité au code source et des droits de copie,  *
+ *  de modification et de redistribution accordés par cette licence, il n'est  *
+ *  offert aux utilisateurs qu'une garantie limitée.  Pour les mêmes raisons,  *
+ *  seule une responsabilité  restreinte pèse  sur l'auteur du programme,  le  *
+ *  titulaire des droits patrimoniaux et les concédants successifs.            *
+ *                                                                             *
+ *  À  cet égard  l'attention de  l'utilisateur est  attirée sur  les risques  *
+ *  associés  au chargement,  à  l'utilisation,  à  la modification  et/ou au  *
+ *  développement  et à la reproduction du  logiciel par  l'utilisateur étant  *
+ *  donné  sa spécificité  de logiciel libre,  qui peut le rendre  complexe à  *
+ *  manipuler et qui le réserve donc à des développeurs et des professionnels  *
+ *  avertis  possédant  des  connaissances  informatiques  approfondies.  Les  *
+ *  utilisateurs  sont donc  invités  à  charger  et  tester  l'adéquation du  *
+ *  logiciel  à leurs besoins  dans des conditions  permettant  d'assurer  la  *
+ *  sécurité de leurs systêmes et ou de leurs données et,  plus généralement,  *
+ *  à l'utiliser et l'exploiter dans les mêmes conditions de sécurité.         *
+ *                                                                             *
+ *  Le fait  que vous puissiez accéder  à cet en-tête signifie  que vous avez  *
+ *  pris connaissance de la licence CeCILL-C, et que vous en avez accepté les  *
+ *  termes.                                                                    *
+ *                                                                             *
+ *  -------------------------------------------------------------------------  *
+ *                                                                             *
+ *  This File is part of the TFT_eSPI_Widgets library.                         *
+ *                                                                             *
+ *  The TFT_eSPI_Widgets library aims to make easy the GUI development using   *
+ *  a Widget approach.                                                         *
+ *                                                                             *
+ *  This software is governed by the CeCILL-C license under French law and     *
+ *  abiding by the rules of distribution of free software. You can use,        *
+ *  modify and/ or redistribute the software under the terms of the CeCILL-C   *
+ *  license as circulated by CEA, CNRS and INRIA at the following URL          *
+ *  "http://www.cecill.info".                                                  *
+ *                                                                             *
+ *  As a counterpart to the access to the source code and rights to copy,      *
+ *  modify and redistribute granted by the license, users are provided only    *
+ *  with a limited warranty and the software's author, the holder of the       *
+ *  economic rights, and the successive licensors have only limited            *
+ *  liability.                                                                 *
+ *                                                                             *
+ *  In this respect, the user's attention is drawn to the risks associated     *
+ *  with loading, using, modifying and/or developing or reproducing the        *
+ *  software by the user in light of its specific status of free software,     *
+ *  that may mean that it is complicated to manipulate, and that also          *
+ *  therefore means that it is reserved for developers and experienced         *
+ *  professionals having in-depth computer knowledge. Users are therefore      *
+ *  encouraged to load and test the software's suitability as regards their    *
+ *  requirements in conditions enabling the security of their systems and/or   *
+ *  data to be ensured and, more generally, to use and operate it in the same  *
+ *  conditions as regards security.                                            *
+ *                                                                             *
+ *  The fact that you are presently reading this means that you have had       *
+ *  knowledge of the CeCILL-C license and that you accept its terms.           *
+ *                                                                             *
+ ******************************************************************************/
+
 #include "widget.h"
 
 using namespace TFT_eSPI_Widgets;
@@ -30,7 +113,7 @@ Widget::Widget(Widget &parent, const Area &area):
   _root(parent._root),
   _parent(parent),
   _child(NULL),
-  _area(area),
+  _area(),
   _default_graph_props(parent._default_graph_props),
   _focus_graph_props(parent._focus_graph_props),
   _need_update(false),
@@ -39,24 +122,14 @@ Widget::Widget(Widget &parent, const Area &area):
   _focus_cb(NULL),
   _unfocus_cb(NULL)
 {
-  if (_area.isEmpty()) {
-    uint8_t b = max(_default_graph_props.getBorderSize(), _focus_graph_props.getBorderSize());
-    _area = parent._area;
-    _area.width -= 2 * b;
-    _area.height -= 2 * b;
-    _area.x += b;
-    _area.y += b;
-  } else {
-    _area.x += parent._area.x;
-    _area.y += parent._area.y;
-  }
+  setArea(area);
   _parent.removeChild();
   _parent._child = this;
 }
 
 Widget::~Widget() {
   removeChild();
-  if (&_root != this) {
+  if (!isRoot()) {
     _parent._child = NULL;
     yield();
   }
@@ -67,6 +140,50 @@ void Widget::removeChild() {
     delete _child;
     _child = NULL;
     yield();
+  }
+}
+
+Area Widget::getArea() const {
+  Area area = _area;
+  if (!isRoot()) {
+    area.x -= _parent._area.x;
+    area.y -= _parent._area.y;
+  }
+  return area;
+}
+
+void Widget::setArea(const Area &area) {
+  if (isRoot()) {
+    if (area.isEmpty()) {
+      TFT_eSPI &tft = getTFT();
+      _area.width = tft.width();
+      _area.height = tft.height();
+      _area.x = 0;
+      _area.y = 0;
+    } else {
+      _area = area;
+    }
+  } else {
+    if (area.isEmpty()) {
+      uint8_t b = max(_default_graph_props.getBorderSize(), _focus_graph_props.getBorderSize());
+      _area = _parent._area;
+      if (_area.width > 2 * b) {
+        _area.width -= 2 * b;
+      } else {
+        _area.width = 0;
+      }
+      if (_area.height > 2 * b) {
+        _area.height -= 2 * b;
+      } else {
+        _area.height = 0;
+      }
+      _area.x += b;
+      _area.y += b;
+    } else {
+      _area = area;
+      _area.x += _parent._area.x;
+      _area.y += _parent._area.y;
+    }
   }
 }
 
@@ -105,8 +222,13 @@ void Widget::draw() {
   TFT_eSPI &tft = getTFT();
   tft.setViewport(_area.x, _area.y, _area.width, _area.height);
   tft.fillScreen(props.getBackgroundColor());
-  if (props.getBorderSize()) {
+  uint8_t border_size = props.getBorderSize();
+  if (border_size) {
     tft.frameViewport(props.getBorderColor(), props.getBorderSize());
+    tft.setViewport(_area.x + border_size,
+                    _area.y + border_size,
+                    _area.width - 2 * border_size,
+                    _area.height - 2 * border_size);
   }
   tft.setTextSize(props.getFontSize());
   tft.setTextColor(props.getFontColor());
--- src/widget.h
+++ src/widget.h
@@ -1,85 +1,85 @@
 /******************************************************************************
-*                                                                             *
-*  Copyright © 2022      -- LIRMM/CNRS/UM                                     *
-*                           (Laboratoire d'Informatique, de Robotique et de   *
-*                           Microélectronique de Montpellier /                *
-*                           Centre National de la Recherche Scientifique /    *
-*                           Université de Montpellier)                        *
-*                                                                             *
-*  Auteurs/Authors: Alban MANCHERON  <alban.mancheron@lirmm.fr>               *
-*                                                                             *
-*  Programmeurs/Programmers:                                                  *
-*                   Alban MANCHERON  <alban.mancheron@lirmm.fr>               *
-*                                                                             *
-*  -------------------------------------------------------------------------  *
-*                                                                             *
-*  Ce fichier fait partie de la librairie TFT_eSPI_Widgets.                   *
-*                                                                             *
-*  La librairie TFT_eSPI_Widgets a pour objet de simplifier le développement  *
-*  d'interfaces graphiques  pour l'uitlisateur  (GUI)  en se basant  sur une  *
-*  approche par *widgets* (composants graphiques).                            *
-*                                                                             *
-*  Ce logiciel est régi par la licence CeCILL-C soumise au droit français et  *
-*  respectant les principes  de diffusion des logiciels libres.  Vous pouvez  *
-*  utiliser, modifier et/ou redistribuer ce programme sous les conditions de  *
-*  la licence CeCILL-C telle que diffusée par le CEA, le CNRS et l'INRIA sur  *
-*  le site "http://www.cecill.info".                                          *
-*                                                                             *
-*  En contrepartie de l'accessibilité au code source et des droits de copie,  *
-*  de modification et de redistribution accordés par cette licence, il n'est  *
-*  offert aux utilisateurs qu'une garantie limitée.  Pour les mêmes raisons,  *
-*  seule une responsabilité  restreinte pèse  sur l'auteur du programme,  le  *
-*  titulaire des droits patrimoniaux et les concédants successifs.            *
-*                                                                             *
-*  À  cet égard  l'attention de  l'utilisateur est  attirée sur  les risques  *
-*  associés  au chargement,  à  l'utilisation,  à  la modification  et/ou au  *
-*  développement  et à la reproduction du  logiciel par  l'utilisateur étant  *
-*  donné  sa spécificité  de logiciel libre,  qui peut le rendre  complexe à  *
-*  manipuler et qui le réserve donc à des développeurs et des professionnels  *
-*  avertis  possédant  des  connaissances  informatiques  approfondies.  Les  *
-*  utilisateurs  sont donc  invités  à  charger  et  tester  l'adéquation du  *
-*  logiciel  à leurs besoins  dans des conditions  permettant  d'assurer  la  *
-*  sécurité de leurs systêmes et ou de leurs données et,  plus généralement,  *
-*  à l'utiliser et l'exploiter dans les mêmes conditions de sécurité.         *
-*                                                                             *
-*  Le fait  que vous puissiez accéder  à cet en-tête signifie  que vous avez  *
-*  pris connaissance de la licence CeCILL-C, et que vous en avez accepté les  *
-*  termes.                                                                    *
-*                                                                             *
-*  -------------------------------------------------------------------------  *
-*                                                                             *
-*  This File is part of the TFT_eSPI_Widgets library.                         *
-*                                                                             *
-*  The TFT_eSPI_Widgets library aims to make easy the GUI development using   *
-*  a Widget approach.                                                         *
-*                                                                             *
-*  This software is governed by the CeCILL-C license under French law and     *
-*  abiding by the rules of distribution of free software. You can use,        *
-*  modify and/ or redistribute the software under the terms of the CeCILL-C   *
-*  license as circulated by CEA, CNRS and INRIA at the following URL          *
-*  "http://www.cecill.info".                                                  *
-*                                                                             *
-*  As a counterpart to the access to the source code and rights to copy,      *
-*  modify and redistribute granted by the license, users are provided only    *
-*  with a limited warranty and the software's author, the holder of the       *
-*  economic rights, and the successive licensors have only limited            *
-*  liability.                                                                 *
-*                                                                             *
-*  In this respect, the user's attention is drawn to the risks associated     *
-*  with loading, using, modifying and/or developing or reproducing the        *
-*  software by the user in light of its specific status of free software,     *
-*  that may mean that it is complicated to manipulate, and that also          *
-*  therefore means that it is reserved for developers and experienced         *
-*  professionals having in-depth computer knowledge. Users are therefore      *
-*  encouraged to load and test the software's suitability as regards their    *
-*  requirements in conditions enabling the security of their systems and/or   *
-*  data to be ensured and, more generally, to use and operate it in the same  *
-*  conditions as regards security.                                            *
-*                                                                             *
-*  The fact that you are presently reading this means that you have had       *
-*  knowledge of the CeCILL-C license and that you accept its terms.           *
-*                                                                             *
-******************************************************************************/
+ *                                                                             *
+ *  Copyright © 2022      -- LIRMM/CNRS/UM                                     *
+ *                           (Laboratoire d'Informatique, de Robotique et de   *
+ *                           Microélectronique de Montpellier /                *
+ *                           Centre National de la Recherche Scientifique /    *
+ *                           Université de Montpellier)                        *
+ *                                                                             *
+ *  Auteurs/Authors: Alban MANCHERON  <alban.mancheron@lirmm.fr>               *
+ *                                                                             *
+ *  Programmeurs/Programmers:                                                  *
+ *                   Alban MANCHERON  <alban.mancheron@lirmm.fr>               *
+ *                                                                             *
+ *  -------------------------------------------------------------------------  *
+ *                                                                             *
+ *  Ce fichier fait partie de la librairie TFT_eSPI_Widgets.                   *
+ *                                                                             *
+ *  La librairie TFT_eSPI_Widgets a pour objet de simplifier le développement  *
+ *  d'interfaces graphiques  pour l'uitlisateur  (GUI)  en se basant  sur une  *
+ *  approche par *widgets* (composants graphiques).                            *
+ *                                                                             *
+ *  Ce logiciel est régi par la licence CeCILL-C soumise au droit français et  *
+ *  respectant les principes  de diffusion des logiciels libres.  Vous pouvez  *
+ *  utiliser, modifier et/ou redistribuer ce programme sous les conditions de  *
+ *  la licence CeCILL-C telle que diffusée par le CEA, le CNRS et l'INRIA sur  *
+ *  le site "http://www.cecill.info".                                          *
+ *                                                                             *
+ *  En contrepartie de l'accessibilité au code source et des droits de copie,  *
+ *  de modification et de redistribution accordés par cette licence, il n'est  *
+ *  offert aux utilisateurs qu'une garantie limitée.  Pour les mêmes raisons,  *
+ *  seule une responsabilité  restreinte pèse  sur l'auteur du programme,  le  *
+ *  titulaire des droits patrimoniaux et les concédants successifs.            *
+ *                                                                             *
+ *  À  cet égard  l'attention de  l'utilisateur est  attirée sur  les risques  *
+ *  associés  au chargement,  à  l'utilisation,  à  la modification  et/ou au  *
+ *  développement  et à la reproduction du  logiciel par  l'utilisateur étant  *
+ *  donné  sa spécificité  de logiciel libre,  qui peut le rendre  complexe à  *
+ *  manipuler et qui le réserve donc à des développeurs et des professionnels  *
+ *  avertis  possédant  des  connaissances  informatiques  approfondies.  Les  *
+ *  utilisateurs  sont donc  invités  à  charger  et  tester  l'adéquation du  *
+ *  logiciel  à leurs besoins  dans des conditions  permettant  d'assurer  la  *
+ *  sécurité de leurs systêmes et ou de leurs données et,  plus généralement,  *
+ *  à l'utiliser et l'exploiter dans les mêmes conditions de sécurité.         *
+ *                                                                             *
+ *  Le fait  que vous puissiez accéder  à cet en-tête signifie  que vous avez  *
+ *  pris connaissance de la licence CeCILL-C, et que vous en avez accepté les  *
+ *  termes.                                                                    *
+ *                                                                             *
+ *  -------------------------------------------------------------------------  *
+ *                                                                             *
+ *  This File is part of the TFT_eSPI_Widgets library.                         *
+ *                                                                             *
+ *  The TFT_eSPI_Widgets library aims to make easy the GUI development using   *
+ *  a Widget approach.                                                         *
+ *                                                                             *
+ *  This software is governed by the CeCILL-C license under French law and     *
+ *  abiding by the rules of distribution of free software. You can use,        *
+ *  modify and/ or redistribute the software under the terms of the CeCILL-C   *
+ *  license as circulated by CEA, CNRS and INRIA at the following URL          *
+ *  "http://www.cecill.info".                                                  *
+ *                                                                             *
+ *  As a counterpart to the access to the source code and rights to copy,      *
+ *  modify and redistribute granted by the license, users are provided only    *
+ *  with a limited warranty and the software's author, the holder of the       *
+ *  economic rights, and the successive licensors have only limited            *
+ *  liability.                                                                 *
+ *                                                                             *
+ *  In this respect, the user's attention is drawn to the risks associated     *
+ *  with loading, using, modifying and/or developing or reproducing the        *
+ *  software by the user in light of its specific status of free software,     *
+ *  that may mean that it is complicated to manipulate, and that also          *
+ *  therefore means that it is reserved for developers and experienced         *
+ *  professionals having in-depth computer knowledge. Users are therefore      *
+ *  encouraged to load and test the software's suitability as regards their    *
+ *  requirements in conditions enabling the security of their systems and/or   *
+ *  data to be ensured and, more generally, to use and operate it in the same  *
+ *  conditions as regards security.                                            *
+ *                                                                             *
+ *  The fact that you are presently reading this means that you have had       *
+ *  knowledge of the CeCILL-C license and that you accept its terms.           *
+ *                                                                             *
+ ******************************************************************************/
 
 #ifndef __WIDGET_H__
 #define __WIDGET_H__
@@ -244,6 +244,10 @@ namespace TFT_eSPI_Widgets {
      * Creates a widget (rectangular area) attached to the given
      * Widget.
      *
+     * The widget graphical properties (background color, line color,
+     * line width, font color, font size) are inherited from the
+     * parent widget.
+     *
      * \param parent The parent widget of the current one. Be aware
      * that if this parent already has a child widget, then this child
      * is automatically destroyed.
@@ -253,9 +257,8 @@ namespace TFT_eSPI_Widgets {
      * border size according to its focus state. The top left anchor
      * of the area is relative to the top left corner of its parent.
      *
-     * The widget graphical properties (background color, line color,
-     * line width, font color, font size) are inherited from the
-     * parent widget.
+     * \remark There is no verification about the area to fit into the
+     * parent area.
      */
     Widget(Widget &parent, const Area &area);
 
@@ -278,18 +281,15 @@ namespace TFT_eSPI_Widgets {
     virtual ~Widget();
 
     /**
-     * Remove current widget child and all its descendants in the
-     * widget tree (if any).
-     */
-    void removeChild();
-
-    /**
-     * Return the widget type (must be overridden by specialized
-     * class)
+     * Interrogate about the current widget being the root of the
+     * widget tree.
      *
-     * \return The concrete widget type is returned.
+     * \return This method returns true if and only if the current
+     * widget is the root of the widget tree.
      */
-    virtual Type getType() const = 0;
+    inline bool isRoot() const {
+      return this == &_root;
+    }
 
     /**
      * Return the root widget.
@@ -311,6 +311,43 @@ namespace TFT_eSPI_Widgets {
     inline Widget &getParent() const {
       return _parent;
     }
+
+
+    /**
+     * Interrogate about some child existence for current widget.
+     *
+     * \return This method returns true if and only if the current
+     * widget has a child.
+     */
+    inline bool hasChild() const {
+      return _child != NULL;
+    }
+
+    /**
+     * Retrieve the child of the current widget.
+     *
+     * By default, the return type is Widget reference, but one can
+     * specify the real type of the widget (no verification is done)
+     * in order to use the specific subtype methods.
+     */
+    template <typename T = Widget>
+    T &getChild() const {
+      return *static_cast<T *>(_child);
+    }
+
+    /**
+     * Remove current widget child and all its descendants in the
+     * widget tree (if any).
+     */
+    void removeChild();
+
+    /**
+     * Return the widget type (must be overridden by specialized
+     * class)
+     *
+     * \return The concrete widget type is returned.
+     */
+    virtual Type getType() const = 0;
 
     /**
      * Return the given widget type as a string.
@@ -352,6 +389,34 @@ namespace TFT_eSPI_Widgets {
     inline const char *getTypeString() const {
       return getTypeString(getType());
     }
+
+    /**
+     * Get the widget area.
+     *
+     * \return Return the widget area. If current widget is the root
+     * of the widget tree, then the top left anchor of the area is
+     * absolute position from the top left corner of the TFT screen ;
+     * otherwise the top left anchor of the is relative to the top
+     * left corner of its parent.
+     */
+    Area getArea() const;
+
+    /**
+     * Set the widget area.
+     *
+     * \param area The area used by the widget. Except for root
+     * widget, the top left anchor of the area is relative to the top
+     * left corner of its parent and if the area dimension is empty,
+     * then use the area of its parent minus its maximal border size
+     * according to its focus state. For the root widget, the area
+     * anchor is the absolute position according to the main TFT
+     * screen top left corner and if the area dimension is empty, then
+     * uses the TFT screen dimensions.
+     *
+     * \remark There is no verification about the area to fit into the
+     * TFT screen or even the parent area.
+     */
+    void setArea(const Area &area);
 
     /**
      * Get the current graphical properties (according to the focus
